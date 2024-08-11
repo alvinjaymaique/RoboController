802fb26ab144437fb72b5554d9ec183fbdeb82b0
alvinjaymaique <148608688+alvinjaymaique@users.noreply.github.com>
2024-08-11 19:29:53 +0800

    Add files via upload

--- roboController/roboController.ino
+++ roboController/roboController.ino
@@ -17,6 +17,8 @@ SoftwareSerial BTSerial(rxPin, txPin); //RX TX
 #define mRight2 7
 #define mRightPwm 6
 
+#define hornPin 8
+
 void run(int a, int b, int speed, int pwm);
 
 void setup() {
@@ -26,81 +28,30 @@ void setup() {
   // Serial.begin(38400);
   // BTSerial.begin(38400); //AT Command Debug mode
 
-  for(int i=2; i<8; i++){
+  for(int i=2; i<9; i++){
     pinMode(i, OUTPUT);
   }
+  // pinMode(9, OUTPUT);
 
   Serial.begin(9600);
   BTSerial.begin(9600);
 }
-
-
-// String receivedData = "";
-// float x_value = 0;
-// float y_value = 0;
-
-// bool isValueX = false;
-// int logic = 0;
-// long prevMillis = 0;
-// void loop() {
-//   // put your main code here, to run repeatedly:
-//   bool isNegative = false;
-  
-
-//   while(BTSerial.available() > 0){
-//     char dataChar = BTSerial.read();
-//     // isValueX = (dataChar=='x')?true:(dataChar=='y')?false:isValueX; 
-//     if(dataChar=='x'){
-//       isValueX = true;
-//       receivedData = "";
-//     }else if(dataChar=='y'){
-//       isValueX = false;
-//     }
-
-//     Serial.println(dataChar);
-
-//     if(dataChar != '\\'){
-//       if (dataChar != '-' && dataChar !='x' && dataChar != 'y') {
-//         receivedData += dataChar;
-//       }else if(dataChar == '-'){
-//         isNegative = true;
-//         Serial.println("The value is negative");
-//       }
-      
-//     }else if(dataChar == '\\'){
-//       if(isValueX){
-//         x_value = receivedData.toFloat();
-//         x_value = (isNegative)?x_value*-1:x_value;
-//         Serial.print("X: ");
-//         Serial.print(isNegative);
-//         Serial.println(x_value);
-//       }else if(!isValueX){
-//         y_value = receivedData.toFloat();
-//         y_value = (isNegative)?y_value*-1:y_value;
-//         Serial.print("Y: ");
-//         Serial.println(y_value);
-//         BTSerial.flush();
-//       }
-      
-//       receivedData = "";
-//       // isValueX = false;
-//       isNegative = false;
-//       break;
-//     }
-//   }
-//   // Serial.print("X: ");
-//   // Serial.println(x_value);
-// }
-
-
 
 String receivedData = "";
 float x_value = 0;
 float y_value = 0;
 bool isValueX = false;
 bool isNegative = false;
+bool isBoostClicked = false;
+String isBoostOn = "";
+int boostValue = 40;
+
+bool isHornClicked = false;
+String isHornOn = "";
+bool activateHorn = false;
+
 // bool isForward = false;
-int maxSpeed = 200;
+int maxSpeed = 180;
 int speed = int(y_value);
 int m1Speed = 0; // Left Motor
 int m2Speed = 0; // Right Motor
@@ -120,7 +71,22 @@ void loop() {
     } else if (dataChar == '-') {
       isNegative = true; 
       // isForward = (!isNegative)?true:false;
-    } else if (dataChar == '\\') {
+    } else if ( dataChar == 'b'){   
+      isBoostClicked = true;
+      Serial.println(isBoostClicked);
+    } else if (isBoostClicked) {
+      isBoostOn = dataChar;
+      // Serial.println(isBoostOn);
+      isBoostClicked = false;
+    } else if (dataChar == 'h'){   
+      isHornClicked = true;
+      Serial.println(isHornClicked);
+    } else if (isHornClicked) {
+      isHornOn = dataChar;
+      // Serial.println(isBoostOn);
+      isHornClicked = false;
+    } 
+    else if (dataChar == '\\') {
       // Process the received data
       float value = receivedData.toFloat();
       value = min(value, 1.00);
@@ -134,13 +100,25 @@ void loop() {
         // Serial.print("Y: ");
         // Serial.println(y_value);
       }
-
       receivedData = "";
       isNegative = false; 
     } else {
       receivedData += dataChar;
     }
     speed = int(y_value*maxSpeed);
+    if(isBoostOn == "t" && speed > 0){
+      speed += boostValue;
+    }else if(isBoostOn == "f" && speed > boostValue){
+      speed -= boostValue;
+    }
+
+
+    if(isHornOn == "t"){
+      activateHorn = true;
+    }else if (isHornOn == "f") {
+      activateHorn = false;
+    }
+
     m1Speed = adjustSpeed(speed, speed*(1-x_value));
     m2Speed = adjustSpeed(speed, speed*(1+x_value));
   }
@@ -151,11 +129,18 @@ void loop() {
   // Serial.println(y_value);
 
 
-  Serial.print("Left Motor Speed: ");
-  Serial.println(m1Speed);
+  // Serial.print("Left Motor Speed: ");
+  // Serial.println(m1Speed);
 
-  Serial.print("Right Motor Speed: ");
-  Serial.println(m2Speed);
+  // Serial.print("Right Motor Speed: ");
+  // Serial.println(m2Speed);
+
+  // if(isHornOn == "t"){
+  //   digitalWrite(hornPin, 1);
+  // }else if (isHornOn == "f") {
+  //   digitalWrite(hornPin, 0);
+  // }
+  digitalWrite(hornPin, activateHorn);
 
   run(mLeft1, mLeft2, m1Speed, mLeftPwm);
   run(mRight1, mRight2, m2Speed, mRightPwm);
@@ -181,15 +166,3 @@ void run(int a, int b, int speed, int pwm) {
   digitalWrite(b, !direction);
   analogWrite(pwm, speed);
 }
-
-// void reverse(int a, int b, int speed, int pwm) {
-//   digitalWrite(a, HIGH);
-//   digitalWrite(b, LOW);
-//   analogWrite(pwm, speed);
-// }
-
-// void forward(int a, int b, int speed, int pwm) {
-//   digitalWrite(a, LOW);
-//   digitalWrite(b, HIGH);
-//   analogWrite(pwm, speed);
-// }
